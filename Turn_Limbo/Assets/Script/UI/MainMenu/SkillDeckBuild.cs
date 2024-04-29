b178fcf42880dadc6d24730519f2ae26a89bd510
jmjeon5533 <jmjeon5533@gmail.com>
2024-04-29 19:28:48 +0900

    #1 Update

--- Turn_Limbo/Assets/Script/UI/MainMenu/SkillDeckBuild.cs
+++ Turn_Limbo/Assets/Script/UI/MainMenu/SkillDeckBuild.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class SelectBtn
+{
+    public List<Image> btns;
+}
 public class SkillDeckBuild : MonoBehaviour
 {
     [SerializeField] private SkillEffect playerSkill;
     [SerializeField] private RectTransform[] skillSelectBtnParent;
     [SerializeField] private Image skillSelectBaseBtn;
+    public SelectBtn[] BtnImage;
     private void Start()
     {
         ReadSpreadSheet.instance.Load(InitSkillSelectBtn);
@@ -18,6 +24,7 @@ public class SkillDeckBuild : MonoBehaviour
         for(int i = 0 ; i < d.SkillList.Count; i++)
         {
             var btn = Instantiate(skillSelectBaseBtn,skillSelectBtnParent[d.SkillList[i].keyIndex]);
+            BtnImage[d.SkillList[i].keyIndex].btns.Add(btn);
             btn.transform.GetChild(0).GetComponent<Image>().sprite = d.SkillList[i].icon;
         }
     }
