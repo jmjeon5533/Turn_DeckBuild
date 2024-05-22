4086b4cbe02b3e72f9a248e4db00916247e7be49
jmjeon5533 <jmjeon5533@gmail.com>
2024-05-23 00:57:08 +0900

    #1 스킬 미할당 조건 수정

--- Turn_Limbo/Assets/Script/UI/MainMenu/PanelMovement.cs
+++ Turn_Limbo/Assets/Script/UI/MainMenu/PanelMovement.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class PanelMovement : MonoBehaviour
 {
+    [SerializeField] SkillEffect playerSkill;
     bool isStage = false;
     bool isMove = false;
+    [SerializeField] Image warningImage;
+    [SerializeField] Text warningText;
+    private void Update()
+    {
+        if (warningImage.color.a > 0 || warningText.color.a > 0)
+        {
+            var value = Mathf.MoveTowards(warningImage.color.a, 0, Time.deltaTime * 0.75f);
+            warningImage.color = new Color(1,1,1,value);
+            warningText.color = new Color(0,0,0,value);
+        }
+    }
+    private void WarningSkillUnSelect()
+    {
+        var value = 0.8f;
+        warningImage.color = new Color(1,1,1,value);
+        warningText.color = new Color(0,0,0,value);
+    }
     public void PanelMove()
     {
-        if(isMove) return;
+        if (isMove) return;
+        if (isStage)
+        {
+            var d = DataManager.instance;
+            int[] keys = new int[3];
+            for (int i = 0; i < playerSkill.SelectIndex.Count; i++)
+            {
+                keys[d.SkillList[playerSkill.SelectIndex[i]].keyIndex]++;
+            }
+
+            if (keys.Any(n => n <= 1))
+            {
+                print("스킬 미할당");
+                WarningSkillUnSelect();
+                return;
+            }
+        }
         isMove = true;
         isStage = !isStage;
-        transform.DOMoveX((isStage ? 1 : 0) * 1920,0.5f).OnComplete(() => isMove = false);
+        transform.DOMoveX((isStage ? 1 : 0) * 1920, 0.5f).OnComplete(() => isMove = false);
     }
 }
