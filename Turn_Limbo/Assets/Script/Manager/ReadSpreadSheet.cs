2f003c60efa4ffe71e7393b948b6bd5dbc7ba4fa
jmjeon5533 <jmjeon5533@gmail.com>
2024-06-04 13:30:00 +0900

    #1 스프라이트 변경 & 가드 추가

--- Turn_Limbo/Assets/Script/Manager/ReadSpreadSheet.cs
+++ Turn_Limbo/Assets/Script/Manager/ReadSpreadSheet.cs
@@ -56,7 +56,7 @@ public class ReadSpreadSheet : MonoBehaviour
             if (!skillDatas.ContainsKey(keyCode))
                 skillDatas.Add(keyCode, new List<Skill>());
 
-            var splitExplain = columns[6].Split('&');
+            var splitExplain = columns[7].Split('&');
             string explain = string.Join("\n", splitExplain);
             var newSkill = new Skill();
             newSkill.index = i;
@@ -69,14 +69,14 @@ public class ReadSpreadSheet : MonoBehaviour
             newSkill.keyIndex = int.Parse(columns[1]) - 1;
             newSkill.actionType = columns[3].EnumParse<Unit.ActionType>();
             newSkill.propertyType = columns[4].EnumParse<Unit.PropertyType>();
-            newSkill.animationName = columns[7];
+            newSkill.animationName = columns[8];
             newSkill.explain = explain;
             newSkill.icon = Resources.Load<Sprite>($"Icon/skill{int.Parse(columns[0])}");
             for (int j = 0; j < 3; j++)
             {
-                newSkill.cost[j] = int.Parse(columns[8 + (j * 3)]);
-                newSkill.minDamage[j] = int.Parse(columns[9 + (j * 3)]);
-                newSkill.maxDamage[j] = int.Parse(columns[10 + (j * 3)]);
+                newSkill.cost[j] = int.Parse(columns[9 + (j * 3)]);
+                newSkill.minDamage[j] = int.Parse(columns[10 + (j * 3)]);
+                newSkill.maxDamage[j] = int.Parse(columns[11 + (j * 3)]);
             }
             skillLists.Add(newSkill);
             skillDatas[keyCode].Add(newSkill);
